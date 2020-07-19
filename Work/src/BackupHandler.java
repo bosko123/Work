f1e2327faa32cfe27857d7732b89e0e098cbff1f
Bosko123 <bostjan.mokrin@gmail.com>
2020-07-19 20:45:23 +0200

    Updated BackupHandler class

--- Work/src/BackupHandler.java
+++ Work/src/BackupHandler.java
@@ -21,7 +21,45 @@ public class BackupHandler {
 		try {
 			
 			bufferedReader = new BufferedReader(new FileReader(file));
-			bufferedWriter = new BufferedWriter(new FileWriter(file.getName() + ".backup", false));
+			bufferedWriter = new BufferedWriter(new FileWriter(new File(file.getName() + ".backup"), false));
+			
+			while (bufferedReader.ready()) {
+				
+				bufferedWriter.write(bufferedReader.readLine() + "\n");
+				
+			}
+			
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+			
+		}
+		finally {
+			
+			try {
+				
+				bufferedReader.close();
+				bufferedWriter.close();
+				
+			} catch (IOException e) {
+				
+				e.printStackTrace();
+				
+			}
+			
+		}
+		
+	}
+	
+	private static void loadBackup(File file) {
+		
+		BufferedReader bufferedReader = null;
+		BufferedWriter bufferedWriter = null;
+		
+		try {
+			
+			bufferedReader = new BufferedReader(new FileReader(new File(file.getName() + ".backup")));
+			bufferedWriter = new BufferedWriter(new FileWriter(file, false));
 			
 			while (bufferedReader.ready()) {
 				
