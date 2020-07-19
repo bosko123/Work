6b4c2f48306d4e07b10eea4fd41021bb616e718f
Bosko123 <bostjan.mokrin@gmail.com>
2020-07-19 19:42:20 +0200

    Added BackupHandler class

---
+++ Work/src/BackupHandler.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class BackupHandler {
+
+	public static void main(String[] args) {
+		
+		makeBackup(new File("C:\\Windows\\System32\\drivers\\etc\\hosts"));
+
+	}
+
+	private static void makeBackup(File file) {
+		
+		BufferedReader bufferedReader = null;
+		BufferedWriter bufferedWriter = null;
+		
+		try {
+			
+			bufferedReader = new BufferedReader(new FileReader(file));
+			bufferedWriter = new BufferedWriter(new FileWriter(file.getName() + ".backup", false));
+			
+			while (bufferedReader.ready()) {
+				
+				bufferedWriter.write(bufferedReader.readLine() + "\n");
+				
+			}
+			
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+			
+		}
+		finally {
+			
+			try {
+				
+				bufferedReader.close();
+				bufferedWriter.close();
+				
+			} catch (IOException e) {
+				
+				e.printStackTrace();
+				
+			}
+			
+		}
+		
+	}
+
+}
