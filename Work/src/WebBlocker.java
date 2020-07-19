b4b97da8fb6f73a5ef06b5a0096b4e5762d1f872
Bosko123 <bostjan.mokrin@gmail.com>
2020-07-19 22:39:27 +0200

    Updated WebBlocker

--- Work/src/WebBlocker.java
+++ Work/src/WebBlocker.java
@@ -1,4 +1,9 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class WebBlocker {
 
@@ -19,9 +24,41 @@ public class WebBlocker {
 		
 	}
 	
-	private static void blockWebPages() {
+	private static void blockWebPages(File file) {
 		
+		BufferedReader bufferedReader = null;
+		BufferedWriter bufferedWriter = null;
 		
+		try {
+			
+			bufferedReader = new BufferedReader(new FileReader("websites-list.txt"));
+			bufferedWriter = new BufferedWriter(new FileWriter(new File(file.getName() + ".backup"), false));
+			
+			while (bufferedReader.ready()) {
+				
+				bufferedWriter.write("127.0.0.1 " + bufferedReader.readLine() + " \n");
+				
+			}
+			
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+			
+		}
+		finally {
+			
+			try {
+				
+				bufferedReader.close();
+				bufferedWriter.close();
+				
+			} catch (IOException e) {
+				
+				e.printStackTrace();
+				
+			}
+			
+		}
 		
 	}
 
